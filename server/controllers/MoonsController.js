2c95f7871f2981b72303737c29d88d3c332deb4c
smhailey <smhailey@msn.com>
2019-07-10 21:54:03 -0600

    all controllers added

--- server/controllers/MoonsController.js
+++ server/controllers/MoonsController.js
@@ -3,11 +3,31 @@ import _moonsService from '../services/MoonsServices'
 
 export default class MoonsController {
 
-  constructor() {
-    this.router = express.Router()
-
+  async createMoon(req, res, next) {
+    try {
+      let moon = await _moonsService.create(req.body)
+      res.send(moon)
+    } catch (err) { next(err) }
   }
 
+  async getMoon(req, res, next) {
+    try {
+      let moon = await _moonsService.findById(req.params.moonId)
+      res.send(moon)
+    } catch (err) { next(err) }
+  }
 
+  async getAllMoons(req, res, next) {
+    try {
+      let moon = await _moonsService.create(req.body)
+      res.send(moon)
+    } catch (err) { next(err) }
+  }
 
+  constructor() {
+    this.router = express.Router()
+      .get('', this.getAllMoons)
+      .get(':/moonId', this.getMoon)
+      .post('', this.createMoon)
+  }
 }
--- server/controllers/PlanetsController.js
+++ server/controllers/PlanetsController.js
@@ -2,12 +2,45 @@ import express from 'express'
 import _planetsService from '../services/PlanetsServices'
 
 export default class PlanetsController {
+  async addMoon(req, res, next) {
+    try {
+      let moon = await _planetsService.findById(req.params.planetId)
+      moon.moons.push(req.body.moonId)
+      await moon.save(err => {
+        if (err) {
+          next(err)
+        }
+      })
+      res.send(moon)
+    } catch (err) { next(err) }
+  }
+
+  async createPlanet(req, res, next) {
+    try {
+      let planet = await _planetsService.create(req.body)
+      res.send(planet)
+    } catch (err) { next(err) }
+  }
+
+  async getPlanet(req, res, next) {
+    try {
+      let planet = await _planetsService.findById(req.params.planetId).populate('moons')
+      res.send(planet)
+    } catch (err) { next(err) }
+  }
+
+  async getAllPlanets(req, res, next) {
+    try {
+      let planet = await _planetsService.create(req.body)
+      res.send(planet)
+    } catch (err) { next(err) }
+  }
 
   constructor() {
     this.router = express.Router()
-
+      .get('', this.getAllPlanets)
+      .get(':/planetId', this.getPlanet)
+      .post('', this.createPlanet)
+      .put(':/planetId/moons/', this.addMoon)
   }
-
-
-
 }
